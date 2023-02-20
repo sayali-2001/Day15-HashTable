cc419cf76b8642e5ac9c021d5620f1e5486225b3
sayali-2001 <60540661+sayali-2001@users.noreply.github.com>
2023-02-20 21:12:37 +0530

    Remove words From the phrase

---
+++ MapMain.java
@@ -0,0 +1,38 @@
+package com.bl.hashmap;
+
+public class MapMain {
+
+    public static void main(String[] args) {
+        String sentence = "Paranoids are not paranoid because they are paranoid but because "
+                + "they keep putting themselves deliberately into paranoid avoidable situations";
+
+        Map<String, Integer> map = new Map<>();
+        String[] words = sentence.toLowerCase().split(" ");
+
+        for (String elements : words) {
+            System.out.println(elements);
+        }
+
+        for (String word : words) {
+            Integer value = map.get(word);
+            if (value == null)
+                value = 1;
+            else
+                value = value + 1;
+            map.add(word, value);
+        }
+
+        map.display();
+        System.out.println();
+
+        for (String word : words) {
+            int frequency = map.get(word);
+            System.out.println("Frequency of " + word + " : " + frequency);
+        }
+
+        System.out.println();
+        System.out.println("Deleting Avoidable : ");
+        map.remove("avoidable");
+        map.display();
+    }
+}
